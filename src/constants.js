b60b34ed0966146be840cb8d71fdbbd88c52cee2
Richard Pius <richardpius2005@gmail.com>
2025-04-13 14:22:00 +0530

    version 1.0 of the 2d game portfolio website!

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. In my PC I like to program cool and amazing 
+    thing in my free time and I am also a Cyber Security enthusist too!
+    You can see my github information by clicking this link: <a href="https://github.com/richard-pius" target="_blank">Github Link</a>`,
+    "cs-degree":  `This is the place where I am going to hang my BCA(Honours) degree because currently I am in 1st year in
+    BCA(Honours) program.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch Youtube videos!`,
+    tv: `That's my TV.`,
+    bed: `This where I sleep. Whenever I lie-down in bed I think about what to 
+    do next and here I come up with exiciting new creative ideas!`,
+    resume: `This is my desk and on it is my resume. You can also Contact me using email at 'richardpius2005@gmail.com'.`,
+    projects: `This 2-D game Portfolio Website is made using html, css, JavaScript, Vite and Kaboom.js .`,
+    library:   `There are a lot of programming book on the shelf!`,
+    exit: `If you wish to exit this game please close the tab 
+    and Thank you for visting my website and I hope that you have a great day ahead!`
+};
